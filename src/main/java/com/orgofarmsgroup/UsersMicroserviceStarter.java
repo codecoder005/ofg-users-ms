7939a1d1ac1b418638a74939e3a98695a01dd2c4
Ramakrishna Janapureddy <developer.raakhi005@gmail.com>
2022-02-10 16:34:16 +0530

    removed unused imports

--- src/main/java/com/orgofarmsgroup/UsersMicroserviceStarter.java
+++ src/main/java/com/orgofarmsgroup/UsersMicroserviceStarter.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.annotation.PostConstruct;
 
